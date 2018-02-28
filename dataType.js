06b7f5b3cff942a7d42825bf8ddecf6bea21ed98
CheviSutt <35573303+CheviSutt@users.noreply.github.com>
2018-02-27 22:51:59 -0700

    Add files via upload
    Good Practice

---
+++ dataType.js
@@ -0,0 +1,15 @@
+var Friday = "Cant wait for the weekend"; //string
+
+var age = 30; //number
+
+var cars = ["Subaru", "Chevrolet"]; //array
+
+var kids = {kidthree: "Cooper", kidkidfour: "Ethan", kidone: "Gavin", kidtwo: "Alexis"}; //object
+
+var mynull = undefined; //null for future use of variable
+
+var casttext = string(cars);
+
+var castbool = number(lightson);
+
+var castnum = number(1);
